2418d258f2f00fc9e9e6248df86070b1c951aa6f
TilapiaKIddo2 <jostenoriop@gmail.com>
2020-05-23 06:52:04 -0600

    ConnectionManager methods ready

--- Aplicación/VentaRepuestos/src/Model/ConnectionManager.java
+++ Aplicación/VentaRepuestos/src/Model/ConnectionManager.java
@@ -7,13 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ConnectionManager {
     private static Connection connection = null;
     private static Statement statement = null;
     private static String IP = "localhost";
     private static String USERNAME = "sa";
-    private static String PASSWORD = "12345";
+    private static String PASSWORD = "2019064588";
     
     public static void logIn(String ip, String username, String password){
         IP = ip;
@@ -40,21 +41,95 @@ public class ConnectionManager {
         return rs;
     }
     
-    public static void insert(String table, ArrayList<String> columns, ArrayList<String> values){
-        
+    public static void insert(String table, ArrayList<String> columns, ArrayList<String> values) throws SQLException{
+        String query = "INSERT INTO " + table + "(";
+        Iterator i = columns.iterator();
+        while (i.hasNext())
+        {
+            query += i.next() + ",";
+        }
+        query = query.substring(0,query.length()-1) + ") VALUES (";
+        i = values.iterator ();
+        while (i.hasNext())
+        {
+            query += i.next() + ",";
+        }
+        query = query.substring(0,query.length()-1) + ");";
+        executeActionQuery(query);
     }
     
-    public static ArrayList<ArrayList<String>> select(ArrayList<String> values, String table, ArrayList<String> conditions){
-        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+    public static ArrayList<ArrayList<Object>> select(ArrayList<String> columns, ArrayList <String> tables) throws SQLException{
+        return select (columns,tables, "");
+    }
+    
+    public static ArrayList<ArrayList<Object>> select(ArrayList<String> columns, ArrayList <String> tables, String conditions) throws SQLException{
+        String query = "SELECT ";
+        Iterator i = columns.iterator();
+        while (i.hasNext())
+        {
+            query += i.next() + ",";
+        }
+        query = query.substring(0,query.length()-1) + " FROM ";
+        i = tables.iterator();
+        while (i.hasNext())
+        {
+            query += i.next() + ",";
+        }
+        query = query.substring(0,query.length()-1);
+        if (!"".equals(conditions)){
+                query += " WHERE " + conditions;
+        }
+        System.out.println(query);
+        ResultSet rs = executeConsultQuery (query);
+        ArrayList<ArrayList<Object>> result = new ArrayList<>();
+        while (rs.next()) 
+        {
+            ArrayList<Object> row = new ArrayList<>();
+            for (String columnLabel : columns) 
+            {
+                row.add(rs.getObject (columnLabel));
+            }
+            result.add (row);
+        }
         return result;
     }
-    
-    public static void update(String table, ArrayList<String> values, ArrayList<String> conditions){
-        
+    public static void update(String table, ArrayList<String> columns, ArrayList<String> values) throws SQLException{
+        update (table,columns,values,"");
     }
     
-    public static void delete(String table, ArrayList<String> conditions){
-        
+    public static void update(String table, ArrayList<String> columns, ArrayList<String> values, String conditions) throws SQLException{
+        String query = "UPDATE " + table + " VALUES ";
+        Iterator it1 = columns.iterator();
+        Iterator it2 = values.iterator();
+        while (it1.hasNext() && it2.hasNext()) 
+        {
+            query += it1.next() + " = " + it2.next() + ",";
+        }
+        query = query.substring(0,query.length()-1);
+        if (!"".equals(conditions)){
+                query += " WHERE " + conditions;
+        }
+        executeActionQuery(query);
+    }
+    
+    public static void deleteTable(String table) throws SQLException{
+        delete (table,"","");
+    }
+    
+    public static void deleteWithConditions(String table, String conditions) throws SQLException{
+        delete (table,"", conditions);
+    }
+    
+    public static void deleteWithSelection(String table, String selection) throws SQLException{
+        delete (table,selection,"");
+    }
+    
+    public static void delete(String table, String selection,String conditions) throws SQLException{
+        String query = "DELETE " + selection +" FROM " + table;
+        if (!"".equals(conditions)){
+                query += " WHERE " + conditions;
+        }
+        executeActionQuery(query);
     }
     
 }
--- Aplicación/VentaRepuestos/src/Model/Main.java
+++ Aplicación/VentaRepuestos/src/Model/Main.java
@@ -6,15 +6,44 @@ import View.MainMenuDisplay;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
 
 public class Main {
     
     public static void main(String[] args){
         try {
-            ConnectionManager.logIn("192.168.100.45", "sa", "12345");
-            ConnectionManager.connect();
-            MainMenuController controller = new MainMenuController(new MainMenuDisplay());
-            
+            ConnectionManager.logIn("192.168.1.10", "sa", "2019064588");
+            ArrayList<String> columns = new ArrayList<String>() { 
+                { 
+                    add("ID_ESTADO"); 
+                } 
+            }; 
+            ArrayList<String> values = new ArrayList<String>() { 
+                { 
+                    add("(SELECT ID FROM ESTADO WHERE TIPO = 'INACTIVO')");
+                } 
+            };
+            ConnectionManager.insert("CLIENTE", columns, values);
+            ArrayList<String> columns2 = new ArrayList<String>() { 
+                { 
+                    add("CEDULA");
+                    add("NOMBRE"); 
+                    add("DIRECCION"); 
+                    add("CIUDAD");
+                    add("ID_CLIENTE"); 
+                } 
+            }; 
+            ArrayList<String> values2 = new ArrayList<String>() { 
+                { 
+                    add("117910466");
+                    add("'Joseph Tenorio'");
+                    add("'Casa color rojo'");
+                    add("'San Jose'");
+                    add("(SELECT MAX(ID) FROM CLIENTE)");
+                } 
+            };
+            ConnectionManager.insert("PERSONA", columns2, values2);
+            MainMenuController controller = new MainMenuController(new MainMenuDisplay()); 
         } catch (SQLException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
